157d5e4a87e6aabc87d80cafa76e4f7428834ba0
Thien-Nhan-28 <21130461@st.hcmuaf.edu.vn>
2024-05-15 21:21:50 +0700

    update add, edit, delete category

--- DHHCeramic/src/main/java/com/fit/nlu/DHHCeramic/controller/admin/category/CateEditController.java
+++ DHHCeramic/src/main/java/com/fit/nlu/DHHCeramic/controller/admin/category/CateEditController.java
@@ -2,8 +2,10 @@ package com.fit.nlu.DHHCeramic.controller.admin.category;
 
 
 import com.fit.nlu.DHHCeramic.model.Category;
+import com.fit.nlu.DHHCeramic.model.Product;
 import com.fit.nlu.DHHCeramic.services.CategoryService;
 import com.fit.nlu.DHHCeramic.services.impl.CategoryServiceImpl;
+import com.fit.nlu.DHHCeramic.services.impl.log.CategoryLogServiceImpl;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -18,6 +20,7 @@ import java.time.LocalDate;
 @WebServlet(urlPatterns = {"/Admin/cate/edit"})
 public class CateEditController extends HttpServlet {
     CategoryService cateService = new CategoryServiceImpl();
+    CategoryLogServiceImpl logService = new CategoryLogServiceImpl();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -44,7 +47,10 @@ public class CateEditController extends HttpServlet {
         category.setStatus(Integer.parseInt(request.getParameter("status")));
         category.setUpdatedBy(request.getParameter("user"));
         category.setUpdatedDate(java.sql.Date.valueOf(LocalDate.now()));
+        String user = request.getParameter("user");
+        Category oldCate = cateService.get(category.getId());
         cateService.edit(category);
+        logService.logCategoryEdit(user, oldCate, category, request);
         System.out.println("Status value: " + category);
         response.sendRedirect(request.getContextPath() + "/Admin/cate/list");
 
